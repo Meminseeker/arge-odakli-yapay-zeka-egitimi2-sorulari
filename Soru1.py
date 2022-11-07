3ea6224abbb169a512cb01edaa9d0b3f7f916edc
Meminseeker <muhammed.arayici@boun.edu.tr>
2022-11-07 21:58:47 +0300

    including streamlit codes and .py file; st codes was not checked

--- Soru1.py
+++ Soru1.py
@@ -23,7 +23,7 @@
 # - [Build Recommender Systems with Movielens Dataset in Python](https://www.codespeedy.com/build-recommender-systems-with-movielens-dataset-in-python/)
 # - [Collaborative Filtering for Movie Recommendations](https://www.kaggle.com/code/faressayah/collaborative-filtering-for-movie-recommendations)
 
-# In[1]:
+# In[7]:
 
 
 import pandas as pd
@@ -31,20 +31,20 @@ import numpy as np
 import os
 
 
-# In[2]:
+# In[8]:
 
 
 os.listdir('datasets')
 
 
-# In[3]:
+# In[9]:
 
 
 movies = pd.read_csv('datasets/movies.csv')
 movies.head(10)
 
 
-# In[4]:
+# In[10]:
 
 
 ratings = pd.read_csv('datasets/ratings.csv')
@@ -53,7 +53,7 @@ ratings.head(10)
 
 # ### Verisetlerini bir araya getirelim. 
 
-# In[5]:
+# In[11]:
 
 
 # MovieID üzerinden kişilerin yorumlarına film isimlerini ve genrelerini ekliyoruz. 
@@ -66,7 +66,7 @@ df.head(10)
 
 # <h4>Average Rating</h4>
 
-# In[6]:
+# In[12]:
 
 
 average_ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
@@ -76,7 +76,7 @@ average_ratings.head(10)
 
 # <h4>Total Number of Ratings</h4>
 
-# In[7]:
+# In[13]:
 
 
 average_ratings['total ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
@@ -86,7 +86,7 @@ average_ratings.head(10)
 
 # <h4>Calculating the Correlation</h4>
 
-# In[8]:
+# In[14]:
 
 
 movie_user = df.pivot_table(index='userId', columns='title', values='rating')
@@ -94,7 +94,7 @@ movie_user = df.pivot_table(index='userId', columns='title', values='rating')
 movie_user.head(10)
 
 
-# In[10]:
+# In[15]:
 
 
 correlations_test = movie_user.corrwith(movie_user['Toy Story (1995)'])
@@ -102,7 +102,7 @@ correlations_test = movie_user.corrwith(movie_user['Toy Story (1995)'])
 correlations_test.head(10)
 
 
-# In[11]:
+# In[17]:
 
 
 recommendation_test = pd.DataFrame(correlations_test, columns=['correlation'])
@@ -114,7 +114,7 @@ recommendation_test.head()
 
 # <h4>Testing the Recommendation System</h4>
 
-# In[12]:
+# In[18]:
 
 
 recc_test = recommendation_test[recommendation_test['total ratings']>100].sort_values('correlation', ascending=False).reset_index()
@@ -126,7 +126,7 @@ recc_test.head(10)
 # 
 # Bundan sonrasında verisetini kullanıp çeşitli ön işlemelerden ve geliştirmelerden sonra alttaki gibi bir fonksiyon oluşturmanızı bekliyoruz. 
 
-# In[13]:
+# In[19]:
 
 
 def film_oner(movie_id):
@@ -150,7 +150,7 @@ def film_oner(movie_id):
     return recommended_movies
 
 
-# In[15]:
+# In[20]:
 
 
 my_movies = film_oner(1)
@@ -158,7 +158,7 @@ for i in range(len(my_movies)):
     print(my_movies[i])
 
 
-# In[16]:
+# In[22]:
 
 
 def isimle_film_oner(movie_name):
@@ -178,10 +178,10 @@ def isimle_film_oner(movie_name):
     return recommended_movies
 
 
-# In[17]:
+# In[23]:
 
 
-my_movie_name = "Twilight (2008)"
+my_movie_name = "Forrest Gump (1994)"
 my_movies = isimle_film_oner(my_movie_name)
 for i in range(len(my_movies)):
     print(my_movies[i])
@@ -197,14 +197,43 @@ for i in range(len(my_movies)):
 # - [How to Collect user inputs with Streamlit](https://www.youtube.com/watch?v=RHzjE-WBaSk)
 # - [8 Best Streamlit Machine Learning Web App Examples in 2022](https://omdena.com/blog/streamlit-web-app-examples/)
 
-# In[18]:
+# In[30]:
 
 
 import streamlit as st
 
 
-# In[ ]:
+# In[31]:
 
 
+st.write("You can either select the name of the movie you like")
+st.write("Also, you can type the id of the movie you like")
 
+
+# <h4>If you want to select the name of the movie you like from a selectbox</h4>
+
+# In[32]:
+
+
+option = st.selectbox("What's the name of the movie you like", df['title'])
+
+
+# In[33]:
+
+
+st.write(isimle_film_oner(option))
+
+
+# <h4>If you want to write the id of the movie you like</h4>
+
+# In[34]:
+
+
+st.text_input("What's the id of the movie you like", key="movie_id")
+
+
+# In[35]:
+
+
+st.write(film_oner(st.session_state.movie_id))
 
